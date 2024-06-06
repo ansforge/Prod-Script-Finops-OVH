4464dcbc549fa423336ca6fea93c529e087b9afd
SSudolski <162603269+SSudolski@users.noreply.github.com>
2024-06-06 09:37:33 +0200

    Update Calcul_inducteurs.py

--- Calcul_inducteurs.py
+++ Calcul_inducteurs.py
@@ -13,16 +13,16 @@ conf.read('scripts_conf.ini')
 conversion={'GW':'Gateway','LB':'Loadbalancer'}
 
 # Chemin du fichier csv inducteurs de sortie (exploité par Power BI)
-csv_ind_projet1=conf['csv_output']['Calcul_inducteurs_projet1']
-csv_ind_projet2=conf['csv_output']['Calcul_inducteurs_projet2']
-csv_ind_projet3=conf['csv_output']['Calcul_inducteurs_projet3']
+csv_ind_SAS_Sante=conf['csv_output']['Calcul_inducteurs_SAS_Sante']
+csv_ind_ProSanteConnect=conf['csv_output']['Calcul_inducteurs_ProSanteConnect']
+csv_ind_SIVIC_SICAP=conf['csv_output']['Calcul_inducteurs_SIVIC_SICAP']
 
 # Chemin du fichier contenant les infos à lire
 csv_instances=conf['csv_output']['Info_instances']
 csv_services=conf['csv_output']['Info_services']
 csv_instances_rules='instances_rules.csv'
 
-def csv_read(csv_file):
+def csv_read(csv_file,logging):
 	return[r for r in csv.DictReader(open(csv_file,'r'))]
 
 def regex1(s):
@@ -35,8 +35,8 @@ def csv_write(csv_file,ct):
 	data_writer.writerow([ct[k]for k in ct])
 
 # Fonction de comptage des inducteurs d'infogérance sur les VM et les services
-# de l'application projet1
-def count_projet1(instance_list,instance_rules_list,service_list):
+# de l'application SAS/Santé.fr
+def count_SAS_Sante(instance_list,instance_rules_list,service_list,logging):
 	ct={}
 	#Comptage selon la convention de nommage des VM
 	for i in instance_list:
@@ -47,7 +47,7 @@ def count_projet1(instance_list,instance_rules_list,service_list):
 						k=r['output']
 						if k in ct:ct[k]+=1
 						else:ct[k]=1
-						print(i['Tenant ID'],';',i['Instance Name'],';',i['Region'],';',k)
+						logging.info([i['Tenant ID'],';',i['Instance Name'],';',i['Region'],';',k])
 						break
 	for service in service_list:
 		if re.search(regex1('..-01-55-*ddd'),service['Service name']):
@@ -56,42 +56,42 @@ def count_projet1(instance_list,instance_rules_list,service_list):
 			else:k='Other'
 			if k in ct:ct[k]+=1
 			else:ct[k]=1
-	print(ct)
-	#Ecriture des données de projet1
-	csv_write(csv_ind_projet1,ct)
+	logging.info([ct])
+	#Ecriture des données de SAS/Santé.fr
+	csv_write(csv_ind_SAS_Sante,ct)
 	return ct
 
 # Fonction de comptage des inducteurs d'infogérance sur les VM de 
-# l'application projet2
-def count_inst_projet2(instance_list):
+# l'application ProSanteConnect
+def count_inst_ProSanteConnect(instance_list,logging):
 	ct={'Prod':0}
 	for i in instance_list:
 		if re.search('(^vps-\w+\.vps\.ovh\.net$)|(^vps\w+\.ovh\.net$)'\
 			,i['Instance ID']):
 			ct['Prod']+=1
-	print(ct)
-	#Ecriture des données de projet2
-	csv_write(csv_ind_projet2,ct)
+	logging.info([ct])
+	#Ecriture des données de ProsantéConnect
+	csv_write(csv_ind_ProSanteConnect,ct)
 	return ct
 
 # Fonction de comptage des inducteurs d'infogérance sur les VM de 
-# l'application projet3
-def count_inst_projet3(instance_list):
+# l'application SIVIC/SICAP
+def count_inst_SIVIC_SICAP(instance_list,logging):
 	ct={'Prod':0}
 	for i in instance_list:
 		if re.search('^vps-\w+\.vps\.ovh\.net$',i['Instance ID']):
 			ct['Prod']+=1
-	print(ct)
-	#Ecriture des données de projet3
-	csv_write(csv_ind_projet3,ct)
+	logging.info([ct])
+	#Ecriture des données de SIVIC/SICAP
+	csv_write(csv_ind_SIVIC_SICAP,ct)
 	return ct
 
-def compute():
+def compute(logging):
 	# Lecture du fichier csv contenant les info sur les instances déployées
-	instance_list=csv_read(csv_instances)
+	instance_list=csv_read(csv_instances,logging)
 	# Lecture du fichier csv contenant les info sur les services déployés
-	instance_rules_list=csv_read(csv_instances_rules)
-	service_list=csv_read(csv_services)
-	count_projet1(instance_list,instance_rules_list,service_list)
-	count_inst_projet2(instance_list)
-	count_inst_projet3(instance_list)
+	instance_rules_list=csv_read(csv_instances_rules,logging)
+	service_list=csv_read(csv_services,logging)
+	count_SAS_Sante(instance_list,instance_rules_list,service_list,logging)
+	count_inst_ProSanteConnect(instance_list,logging)
+	count_inst_SIVIC_SICAP(instance_list,logging)
